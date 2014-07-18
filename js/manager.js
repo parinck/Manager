cf10bd9bfa2d9a57495adbe3bcf83f163b48f301
parin2092 <parin2092@gmail.com>
2014-07-19 02:39:16 +0530

    changes

--- js/manager.js
+++ js/manager.js
@@ -1,18 +1,10 @@
 var Manager = angular.module( 'Manager', [] );
 
-var ext = {
-	name: "",
-	shortName: "",
-	description: "",
-
-}
-
 function ManagerOnDuty( $scope ) {
 	function getExtensionList() {
 		chrome.management.getAll( function ( extensions ) {
 			$scope.$apply( function () {
 				$scope.extensions = extensions;
-				console.log( $scope.extensions );
 			} )
 		} )
 	};
@@ -29,7 +21,7 @@ function ManagerOnDuty( $scope ) {
 	$scope.onOff = function ( extension ) {
 		chrome.management.setEnabled( extension.id, !extension.enabled, function () {
 			$scope.$apply( function () {
-				getExtensionList();
+				extension.enabled = !extension.enabled;
 			} )
 		} );
 	}
